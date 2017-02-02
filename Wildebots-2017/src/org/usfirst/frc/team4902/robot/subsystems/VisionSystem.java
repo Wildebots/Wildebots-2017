f45e83ef010fa365330b5dda6b178422996f5c5c
1retidav <1retidav@RBH28214.ihdsb.CA>
2017-02-02 15:28:07 -0500

    Initial vision system test

--- Wildebots-2017/src/org/usfirst/frc/team4902/robot/subsystems/VisionSystem.java
+++ Wildebots-2017/src/org/usfirst/frc/team4902/robot/subsystems/VisionSystem.java
@@ -1,11 +1,11 @@
 package org.usfirst.frc.team4902.robot.subsystems;
 import org.opencv.core.Mat;
-
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
 *The vision system controls camera input and image processing,
@@ -15,7 +15,7 @@ public class VisionSystem {
 
 	private static CameraServer cServer;
 	private static UsbCamera cam0;
-	private static Mat matIn, matOut;
+	private static Mat matIn, matOut, matEdges;
 	private static CvSink iStream;
 	private static CvSource oStream;
 	private static Thread thread;
@@ -23,20 +23,25 @@ public class VisionSystem {
 	public static void start() {
 		cServer = CameraServer.getInstance();
 		cam0 = cServer.startAutomaticCapture();
-//		cam0.setResolution(640, 480);
 					
-//		matIn = new Mat();
-//		matOut = new Mat();
-//		
-//		iStream = cServer.getVideo();
-//		oStream = cServer.putVideo("USB Camera 0", 640, 480);
-//		 thread = new Thread(() -> {
-//			while (true) {
-//				iStream.grabFrame(matIn);
-//				oStream.putFrame(matOut);
-//			}
-//		});
-//		 thread.start();
+		matIn = new Mat();
+		matOut = new Mat();
+		
+		iStream = cServer.getVideo();
+		oStream = cServer.putVideo("USB Camera 0", 640, 480);
+		
+		thread = new Thread(() -> {
+			while (true) {
+				iStream.grabFrame(matIn);
+				
+				Imgproc.GaussianBlur(matIn, matOut, new Size(5.0f, 5.0f), 0.0);
+				Imgproc.Canny(matOut, matEdges, 100, 200);
+				matOut = matEdges;
+				
+				oStream.putFrame(matOut);
+			}
+		});
+		
+		thread.start();
 	}
-	
 }
